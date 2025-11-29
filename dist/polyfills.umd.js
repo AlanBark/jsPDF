451ee9724b39e9f2fc66e55f4d0f6994a4cc8fb3
AlanBarkes <testify7982@gmail.com>
2025-11-30 01:15:17 +1000

    rebuild dist with fixes

--- dist/polyfills.umd.js
+++ dist/polyfills.umd.js
@@ -1,7 +1,7 @@
 /** @license
  *
  * jsPDF - PDF Document creation from JavaScript
- * Version 3.0.4 Built on 2025-11-19T12:48:37.233Z
+ * Version 3.0.4 Built on 2025-11-29T15:13:51.277Z
  *                      CommitID 00000000
  *
  * Copyright (c) 2010-2025 James Hall <james@parall.ax>, https://github.com/MrRio/jsPDF
